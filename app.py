16c82ffde4734b1c98dfb94406bb40d2e8709379
Alexandra Kontogianni <sdi2400088@di.uoa.gr>
2026-05-05 20:22:04 +0300

    changes in question 1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pymysql
-from bottle import route, run
+from bottle import route, run,
+template, request, static_file
 
 connection = pymysql.connect(
 host='localhost',
@@ -9,8 +10,13 @@ database = 'flights'
 )
 
 
-@route('/')
-def index():
-    return "Η συνδεση με την βαση ειναι ετοιμη."
+@route('/findAirlineByAge/<x>/<y>')
+def find_airline(x,y):
+    cursor = connection.cursor()
+    cursor.execute("SELECT airline_id, COUNT(*) FROM passengers WHERE age BETWEEN %s AND %s GROUP BY airline_id", (x,y))
+    result = cursor.fetchall()
+    return str(result) 
 
-run(host='localhost' , port=8080, debug = True)
+    run(host='localhost', port=8080, debug=True)
+
+    
